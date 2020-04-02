7956d0370aac62f61d37223aa2377e2e6c81e12a
hilton@kri8it.com <hilton@kri8it.com>
2020-04-02 10:17:24 +0200

    Version 1.0.1

--- fincon-woocommerce.php
+++ fincon-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Fincon For Woocommerce
  * Plugin URI:        https://www.kri8it.com
  * Description:       Connects your Fincon accounting system WebAPI to Woocommerce.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Hilton Moore
  * Author URI:        https://www.kri8it.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'FINCON_WOOCOMMERCE_VERSION', '1.0.0' );
+define( 'FINCON_WOOCOMMERCE_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
--- includes/class-fincon-woocommerce.php
+++ includes/class-fincon-woocommerce.php
@@ -70,7 +70,7 @@ class Fincon_Woocommerce {
 		if ( defined( 'FINCON_WOOCOMMERCE_VERSION' ) ) {
 			$this->version = FINCON_WOOCOMMERCE_VERSION;
 		} else {
-			$this->version = '1.0.0';
+			$this->version = '1.0.1';
 		}
 		$this->plugin_name = 'fincon-woocommerce';
 
@@ -200,7 +200,7 @@ class Fincon_Woocommerce {
 
 		$plugin_admin = new Fincon_Woocommerce_Admin( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action('plugins_loaded', $plugin_admin, 'check_for_update' );
+		$this->loader->add_action( 'plugins_loaded', $plugin_admin, 'check_for_update' );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
